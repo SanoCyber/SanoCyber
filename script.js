619a88396728493f1a29cfea720910d296d58d69
SanoCyber <glen.grevatt8855@gmail.com>
2025-01-03 21:05:34 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ if (yearSpan) {
   yearSpan.textContent = new Date().getFullYear();
 }
 
-// Smooth scrolling for nav links (optional, as we've used 'scroll-behavior: smooth' in CSS)
+// Smooth scrolling for nav links
 const navLinks = document.querySelectorAll(".nav-links a");
-
 navLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
+    // Scroll smoothly to the section
     document.querySelector(link.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
     });
-    // Close mobile nav after click
+    // Close the mobile nav after clicking a link
     document.getElementById("navLinks").classList.remove("show");
     // Update active link class
     navLinks.forEach((navItem) => navItem.classList.remove("active-link"));
